e972935db4c451e034b559636c5b25ca3565bde2
jayakumar <jeyakumar.m@zohocorp.com>
2016-02-04 11:16:26 +0530

    data print change

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('user');
+  this.route('user', function() {
+    this.route('register');
+  });
 });
 
 export default Router;
