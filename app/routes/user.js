8a3c2cc36457365f9648fd752cd0f0a9134509cc
jayakumar <jeyakumar.m@zohocorp.com>
2016-02-04 11:14:39 +0530

    data print change

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,10 +1,17 @@
 import Ember from 'ember';
-var user=[{
-  name: 'jeyakumar',
-  number: 1009092
-}]
+var user=[],i=0;
+var person={};
+person.uName="aaabbb";
+person.ind=1;
+person.rNo=1;
+user[i]=person;
+i++;
+
+var temp=Ember.Component.extend;
+
 export default Ember.Route.extend({
-  model(){
-    return user;
+  model:function(){
+      console.log(user);
+      return user;
   }
 });
