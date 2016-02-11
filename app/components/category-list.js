0d23d6f7f060a3426173da7ab102bddf7a11eeb8
jayakumar <jeyakumar.m@zohocorp.com>
2016-02-11 15:41:09 +0530

    Rm unwanted files and small change in edit

--- app/components/category-list.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  willRender(){
-  this.set('categories',{'animal':['dog','cow','goat'], 'bird':['sparrow','crow','peacock']});
-}});
