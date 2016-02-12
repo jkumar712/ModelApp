7c6f00432db06e4bf25d5ecf9405a9c8b10c03ac
dkdrk <dkdrk452@gmail.com>
2016-02-12 19:43:19 +0530

    commiting after removing unwanted file

--- app/routes/library/detail.js
+++ app/routes/library/detail.js
@@ -1,16 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-<<<<<<< HEAD
-=======
 
->>>>>>> 5886d14651269318f80d1bff9ac029196f0ef470
-  model(params)
   {
     return this.store.findRecord('library',params.no);
   }
-<<<<<<< HEAD
-=======
 
->>>>>>> 5886d14651269318f80d1bff9ac029196f0ef470
 });
